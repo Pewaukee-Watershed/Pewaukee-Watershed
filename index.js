3c96003d94907142cb892c7ef3084b5c2b70978e
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2020-12-07 11:14:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,12 @@ console.log(process.env)
 console.log('Finding Files')
 console.time('find');
 (async () => {
-  const globber = await glob.create('**/*.md\n!**/node_modules')
+  const globber = await glob.create('**/*.jsc\n!**/node_modules')
   const files = await globber.glob()
   
   await Promise.all(files.map(async file => {
     const text = await fs.readFile(file, 'utf8')
-    await fs.writeFile(file, text + '\nAnother line')
+    console.log(file, text)
   }))
 })()
   .then(() => {
