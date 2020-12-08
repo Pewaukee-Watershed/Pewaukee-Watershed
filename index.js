c3bc964070bc3f2b038b2c6df36fb4f2a89a83cf
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2020-12-08 09:35:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const babel = require('@babel/core')
 const fs = require('fs').promises
 
 console.log('Finding Files')
-console.time('find');
+console.time('transform');
 (async () => {
+  const octokit = github.getOctokit(process.env.GITHUB_TOKEN)
+  
   const reactPreset = babel.createConfigItem('@babel/preset-react', { type: 'preset' })
   
   const globber = await glob.create('**/*.jsx\n!**/node_modules')
@@ -20,10 +22,17 @@ console.time('find');
       presets: [reactPreset]
     })
     console.log(code)
+    const blob = await octokit.git.createBlob({
+      owner: github.context.repo.owner,
+      repo: github.context.repo.repo,
+      content: content,
+      encoding: 'utf-8'
+    })
+    console.log(blobl.data)
   }))
 })()
   .then(() => {
-    console.timeEnd('find')
+    console.timeEnd('transform')
   })
   .catch(e => {
     core.setFailed(e)
