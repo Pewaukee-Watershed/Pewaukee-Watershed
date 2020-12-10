07605596bae8d0380140072ddb189588baba5af9
KhajasJS <57632215+KhajasJS@users.noreply.github.com>
2020-12-10 07:14:41 -0600

    Import react

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ console.time('transform');
       encoding: 'utf-8'
     })
     const jsFile = file.replace('.jsx', '.js')
-    await fs.writeFile(jsFile, code)
+    await fs.writeFile(jsFile, `import React from 'react'\n${code}`)
     const { default: App } = await import(jsFile)
     console.log(App)
     console.log(React.createElement(App))
